abf5d6147c81b5a48c573fe972835f574ffeaf5c
Karolina Hamerszmidt <khamerszmidt@gmail.com>
2020-06-06 15:58:46 +0200

    Updates

---
+++ precache-manifest.211c52ab5066affe87c63b56c58ad91d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bc3bcb8c82222f968427650c2e40dc",
+    "url": "/kwiatki-dzienniczek/index.html"
+  },
+  {
+    "revision": "fef61e7869d98e0a8322",
+    "url": "/kwiatki-dzienniczek/static/css/main.28091250.chunk.css"
+  },
+  {
+    "revision": "90d6963212200f1b4d45",
+    "url": "/kwiatki-dzienniczek/static/js/2.3fbd21c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kwiatki-dzienniczek/static/js/2.3fbd21c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef61e7869d98e0a8322",
+    "url": "/kwiatki-dzienniczek/static/js/main.d99191e6.chunk.js"
+  },
+  {
+    "revision": "4f404416f4063522d76c",
+    "url": "/kwiatki-dzienniczek/static/js/runtime-main.dfaf6ec2.js"
+  }
+]);
